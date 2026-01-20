9886d66afaf668d45868180639e6bcec9b880a49
shivanigpavaskar <pshivani@trellissoft.ai>
2026-01-20 17:14:01 +0530

    updated

--- src/core/apis/axios.tsx
+++ src/core/apis/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = window.atob(import.meta.env.VITE_API_BASE_URL);
+export const API_BASE_URL = "https://api.conversational-dev.trellissoft.ai";
 console.log("API_BASE_URL", API_BASE_URL);
 export default axios.create({
   baseURL: API_BASE_URL,
